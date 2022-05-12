cd045c5fb273eece654a07857f6a5d038223c48e
artem <zalivonka02@gmail.com>
2022-05-12 20:10:31 +0300

    first

--- reverse_string.go
+++ reverse_string.go
@@ -1,8 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	for i := range input {
-		output = output + string(input[len(input)-1-i])
+	runes := []rune(input)
+
+	for i := 0; i < len(runes)/2; i++ {
+		j := len(runes) - i - 1
+		runes[i], runes[j] = runes[j], runes[i]
 	}
+
+	output = string(runes)
 	return output
 }
